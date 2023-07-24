a67314630789d8033849f7b30dbffc4e780f7e35
Ivy <ivystacyn@gmail.com>
2023-07-24 19:27:58 +0000

    Task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Returns information about employee's
+TODO list progress in JSON format
+"""
+from json import dump
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users"
+
+    users = get(url + "users").json()
+
+    dict = {}
+    for user in users:
+        user_id = user.get("id")
+        username = user.get("username")
+        url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+        url = url + "todos"
+        response = requests.get(url)
+        tasks = response.json()
+        dict[user_id] = []
+        for task in tasks:
+            dict[user_id].append({
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": username
+            })
+    with open("todo_all_employees.json", "w") as file:
+        dump(dict, file)
