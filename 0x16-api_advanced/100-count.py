abe8b3a1633a05a52a05574ab62cc3b01ea0a3f3
Ivy <ivystacyn@gmail.com>
2023-08-08 17:10:38 +0000

    Task 3

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -4,10 +4,10 @@ Queries the Reddit API, parses the title of all hot articles,
 and prints a sorted count of given keywords.
 """
 
-import requests
-from collections import Counter
+from requests import get
 
-def count_words(subreddit, word_list, key_words={}, after=None, counts={}):
+
+def count_words(subreddit, word_list, key_words={}, count=(), after=None):
     """
     Recursively queries the Reddit API to count occurrences
     of keywords in hot article titles for a given subreddit.
@@ -16,33 +16,37 @@ def count_words(subreddit, word_list, key_words={}, after=None, counts={}):
         dict: A dictionary containing keyword counts.
         None if no results are found.
     """
-    if not counts:
+    if not count:
         key_words = {word.lower(): 0 for word in word_list}
-        counts = Counter(word.lower()for word in word_list)
+        word_list = [word.lower() for word in word_list]
+        count = {word.lower(): word_list.count(word.lower())
+                 for word in word_list}
 
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     params = {"limit": 100, "after": after}
     headers = {"User-Agent": "MyUserAgent"}
 
-    response = requests.get(url, headers=headers, params=params,
+    response = get(url, headers=headers, params=params,
                             allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()
-        children = data["data"]["children"]
+        data = response.json().get('data')
+        after = data.get("after")
+        posts = data.get("children")
 
-        for post in children:
-            title = post["data"]["title"].lower()
+        #  get count of key words in each title
+        for post in posts:
+            title = post.get("data").get("title").lower()
             words = title.split()
             for word in key_words.keys():
                 key_words[word] += words.count(word)
 
-        after = data["data"]["after"]
+        #  # Recursively call the function with the 'after' token for pagination
         if after:
-            return count_words(subreddit, word_list, key_words, after, counts)
+            return count_words(subreddit, word_list, key_words, count, after)
         else:
             for key in key_words.keys():
-                key_words[key] *= counts[key]
+                key_words[key] *= count[key]
             key_words = sorted(key_words.items(),
                                key=lambda item: (-item[1], item[0]))
             for item in key_words:
