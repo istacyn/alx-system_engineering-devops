73236858eccf73e36e35e7feea18ee1e317c4e06
Ivy <ivystacyn@gmail.com>
2023-08-09 08:12:17 +0000

    Web debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Automate apache configuratiom fix
+exec { 'config_file':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path => '/bin'
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,3 @@
+Web stack debugging #3
+
+Debuggind a Wordpress website running on a LAMP stack whose Apache is returning a 500 error.
